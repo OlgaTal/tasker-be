2bea3bd1559fb2905f7a3ccdbd6bec719ae4066a
Olga Talovikova and Steven Snow <pair+olga+steven@allstate.com>
2016-08-17 15:10:59 -0500

    added cross origin and database keepalive pings

--- src/main/java/com/tasker/controllers/TasksController.java
+++ src/main/java/com/tasker/controllers/TasksController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping({"/tasks"})
 public class TasksController {
     @Autowired
